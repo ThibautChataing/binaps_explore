ce091951cb157bd24e548a6ea23856305aa0b2c6
Thibaut Chataing <thibaut.chataing@outlook.fr>
2022-09-23 11:37:41 +0200

    scrap github data'

--- github_explore/github_dl.py
+++ github_explore/github_dl.py
@@ -1,20 +1,48 @@
 import logging
 import os
+import datetime
 from unittest import skip
 import tqdm
+import json
 import pandas as pd
+import time
 from enum import Enum
+import requests
 
 from github import Github
 
-"""
-def : 
-    event = Issue alone / PR alone / PR from issue
-Idea :
-for each repo :
-    ddl (event, participants)
-        
-"""
+def set_logger(file_name=None, log_level=logging.DEBUG):
+    # create logger for prd_ci
+    """
+    Args:
+        file_name: name for the file to log (full path and extension ex: output/run.log)
+        :param log_level:
+    """
+    log = logging.getLogger('main')
+    log.setLevel(level=logging.DEBUG)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('[%(levelname)s] %(module)s in %(funcName)s at %(lineno)dl : %(message)s')
+
+    if file_name:
+        # create file handler for logger.
+        fh = logging.FileHandler(file_name, mode='w+')
+        fh.setLevel(level=logging.DEBUG)
+        fh.setFormatter(formatter)
+    # reate console handler for logger.
+    ch = logging.StreamHandler()
+    ch.setLevel(level=logging.ERROR)
+    ch.setFormatter(formatter)
+
+    # add handlers to logger.
+    if file_name:
+        log.addHandler(fh)
+
+    log.addHandler(ch)
+    log.setLevel(level=logging.DEBUG)
+
+
+    return log
 
 class EventType(Enum):
     ONLYISSUE = 0
@@ -45,33 +73,55 @@ class ReposStatus(Enum):
     DOING = 1
     DONE = 2
 
+def check_remaining_request():
+    log = logging.getLogger('main')
+
+    r = requests.get('https://api.github.com/users/octocat')
+    remaining = int(r.headers['X-RateLimit-Remaining'])
+    log.debug(f"Still {remaining} requests")
+    if remaining < 4:
+        log.critical(f'Time to sleep or change internet {remaining} requests')
+        #i = input("Sleep or change IP ? 0/1")
+        i = '0'
+        if i == '0':
+            time.sleep(60*60+1)
+        elif i == '1':
+            check_remaining_request()
+        else:
+            pass
+
 def get_from_named_user(named_user):
     return (named_user.id, named_user.login)
 
 def get_event_from_pr(pr, repo):
+    check_remaining_request()
     if pr.as_issue():
         typ = EventType.ISSUEPR.value
     else:
         typ = EventType.ONLYPR.value
     ev  = Event(repo=repo, id=pr.id, etype=typ)
 
+    check_remaining_request()
     ev.participants.add(get_from_named_user(pr.user))
 
     assignes = tuple(get_from_named_user(user) for user in pr.assignees)
     if assignes:
         ev.participants.add(assignes)
 
+    check_remaining_request()
     # comments
     if pr.comments:
         com = pr.get_comments()
         for c in com:
             ev.participants.add(get_from_named_user(c.user))
+
+    check_remaining_request()
     if pr.review_comments:
         com = pr.get_review_comments()
         for c in com:
             ev.participants.add(get_from_named_user(c.user))
 
-
+    check_remaining_request()
     com = pr.get_issue_comments()
     if com.totalCount:
         for c in com:
@@ -80,69 +130,94 @@ def get_event_from_pr(pr, repo):
     return ev
 
 
-def main():
-    df = pd.DataFrame(columns=['repo', 'id', 'event_type', 'participants'])
-    with open(r'C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\repos_name.txt', 'r') as fd:
+def main(root):
+    log = logging.getLogger('main')
+
+    whole_repo = os.path.join(root, r'repos_name.txt')
+    repo_missing_path =  os.path.join(root, r'repos_name_missing.txt')
+    data_path =  os.path.join(root, r'github_scrap_data.json')
+
+    if os.path.isfile(repo_missing_path):
+        repo_todo = repo_missing_path
+        df = pd.read_json(data_path)
+    else:
+        repo_todo = whole_repo
+        df = pd.DataFrame(columns=['repo', 'id', 'event_type', 'participants'])
+    
+    with open(repo_todo, 'r') as fd:
         repos = fd.read()
     repos = repos.split(' ')
 
     g = Github()  # init github conenction
-    for repo in repos:  # iterate over all repos
-        rep = g.get_repo(repo)
-        """ prs = rep.get_pulls(state='all')  # get all pr
-        for pr in prs:
-            
-            ev = get_event_from_pr(pr, repo)
-            df = pd.concat([df, ev.to_dataframe()]) """
+    try:
+        for repo in tqdm.tqdm(repos,desc="Repos", leave=True, position=2):  # iterate over all repos
+            check_remaining_request()
+            log.info(f'Doing repo {repo}')
+            rep = g.get_repo(repo)
 
-        issues = rep.get_issues(state='all')
-        for iss in issues:
-            ev  = Event(repo=repo, id=iss.id)
+            check_remaining_request()
+            prs = rep.get_pulls(state='all')  # get all pr
+            for pr in tqdm.tqdm(prs, desc="PR", leave=True, position=3):
+                check_remaining_request()
+                ev = get_event_from_pr(pr, repo)
+                df = pd.concat([df, ev.to_dataframe()])
 
-            pr = iss.pull_request
-            if pr:
-                ev.etype = EventType.ISSUEPR.value
-                pr = iss.as_pull_request()
-                if pr.id in df.id:
-                    continue
+            check_remaining_request()
+            issues = rep.get_issues(state='all')
+            for iss in tqdm.tqdm(issues,desc="Issue", leave=True, position=3):
+                ev  = Event(repo=repo, id=iss.id)
+
+                pr = iss.pull_request
+                if pr:
+                    ev.etype = EventType.ISSUEPR.value
+                    check_remaining_request()
+                    pr = iss.as_pull_request()
+                    if pr.id in df.id:
+                        continue
+                    else:
+                        check_remaining_request()
+                        evpr = get_event_from_pr(pr, repo)
+                        ev.participants = evpr.participants
+
                 else:
-                    evpr = get_event_from_pr(pr, repo)
-                    ev.participants = evpr.participants
+                    ev.etype = EventType.ONLYISSUE.value
+                    check_remaining_request()
+                    ev.participants.add(get_from_named_user(iss.user))
+                    check_remaining_request()
+                    assignes = tuple(get_from_named_user(user) for user in iss.assignees)
+                    if assignes:
+                        ev.participants.add(assignes)
 
-            else:
-                ev.etype = EventType.ONLYISSUE.value
-                
-                ev.participants.add(get_from_named_user(iss.user))
-
-                assignes = tuple(get_from_named_user(user) for user in iss.assignees)
-                if assignes:
-                    ev.participants.add(assignes)
-
-                # comments
-                com = iss.get_comments()
-                for c in com:
-                    ev.participants.add(get_from_named_user(c.user))
-
-
+                    # comments
+                    check_remaining_request()
+                    com = iss.get_comments()
+                    for c in com:
+                        check_remaining_request()
+                        ev.participants.add(get_from_named_user(c.user))
+            log.info(f'{repo} done, saving it')
             df = pd.concat([df, ev.to_dataframe()])
-            break
-        break
+            repo_name = repo.replace('\\', '_')
+            ev.to_dataframe().to_json(os.path.join(root, f'save_{repo}.json'))
+
+    except Exception as e:
+
+            log.error(e)
+            repos_missing = set(repos) - set(df.repo.unique()) 
+            repos_missing.add(repo)
+            log.info(f'saving repo todo and current data')
+            with open(repo_missing_path, 'w') as fd:
+                fd.write(' '.join(repos_missing))
+            df.reset_index(inplace=True, drop=True)
+            df.to_json(data_path)
+            
 
 if __name__ == "__main__":
-    logging.basicConfig()
-
-    logging.info("start")
-    main()
-    #g = Github()
-    #rep = g.get_repo(repos[0])
-    #pr = rep.get_pulls(state="all")
-    #print(pr.totalCount)
-    # for pr
-    # pr.as_issue()
-    # reviewer
-    # assignees
-    # comments  (get_comments / get_issue_comments)
-    # user
-
-    logging.info("end")
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%Hh%Mm%Ss")
+    root = fr'C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore'
+    log_file = os.path.join(root, fr'github_dl_{now}.log')
+    set_logger(log_file)
+    log = logging.getLogger('main')
+    log.info("start")
+    main(root)
+    log.info("end")
 
