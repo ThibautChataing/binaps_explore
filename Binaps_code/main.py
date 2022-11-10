399800c7ac68b340f743aa0c38e62e1d979d6bd7
Thibaut Chataing <thibaut.chataing@outlook.fr>
2022-11-10 10:29:52 +0100

    dumb oversight in main

--- Binaps_code/main.py
+++ Binaps_code/main.py
@@ -146,33 +146,5 @@ if __name__ == '__main__':
 
     # data is going from one to max
     # pattern is goind from 0 to max-1
-    #argument = r"-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_only_versicolor.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10"
-    #argument = r"-i C:\Users\Thibaut\Documents\These\code\binaps_explore\Data\accidents.dat -o ./output --epochs 20 --batch_size 1000 --test_batch_size 100"
-    
-    
-#     exp = ['-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_only_setosa.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_only_versicolor.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_only_virginica.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_set_ver.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_set_vir.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_setosa.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\Iris_vir_ver.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 24 --test_batch_size 10',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\credit_card.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 1000 --test_batch_size 100',
-#  '-i C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\input\\synthetic_data_1000_1000_10_0.001_INTER_2022-08-29T16h20m33s.dat -o C:\\Users\\Thibaut\\Documents\\These\\code\\experiments\\output\\binaps --epochs 20 --batch_size 100 --test_batch_size 10']
-#     for e in exp:
-#         main(e.split())
-    #os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:256'
 
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\data_scrap\v2\github_binary_event_filter_part.dat -o C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\data_scrap\ --epochs 150 --hidden_dim 1000 --log_interval 100"
-    #argument = rf'-i C:\Users\Thibaut\Documents\These\code\experiments\synth_simplest\data\synthetic_data_100000_1000_100_0.0_NO_INTER_2022-10-27T14h38m21s.dat -o C:\Users\Thibaut\Documents\These\code\experiments\synth_simplest\output --lr 0.02 --epochs 50 --batch_size 64 --test_batch_size 64 --log_interval 100' 
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\experiments\exp2\input\Iris_setosa_v2.dat -o C:\Users\Thibaut\Documents\These\code\experiments\exp2\output\ --epochs 150 --batch_size 25 --test_batch_size 25"
-    #argument = r"-i C:\Users\Thibaut\Documents\These\code\experiments\exp2\input\Iris_only_setosa_v2.dat -o C:\Users\Thibaut\Documents\These\code\experiments\exp2\output\ --epochs 150 --batch_size 2 --test_batch_size 25"
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\data_scrap\v3\github_binary_event_filter_part.dat -o C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\data_scrap\v3\out --epochs 600 --hidden_dim 1000 --log_interval 100"
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\data_scrap\v3\github_binary_event_filter_part.dat -o C:\Users\Thibaut\Documents\These\code\binaps_explore\github_explore\data_scrap\v3\out --epochs 600 --hidden_dim 1000 --log_interval 100"
-    
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\experiments\exp3\input\Iris_versicolor.dat -o C:\Users\Thibaut\Documents\These\code\experiments\exp3\output\ --epochs 150 --batch_size 25 --test_batch_size 25"
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\experiments\exp3\input\Iris_vir_ver.dat -o C:\Users\Thibaut\Documents\These\code\experiments\exp3\output\ --epochs 150 --batch_size 25 --test_batch_size 25"
-    argument = r"-i C:\Users\Thibaut\Documents\These\code\experiments\exp3\input\Iris_only_versicolor.dat -o C:\Users\Thibaut\Documents\These\code\experiments\exp3\output\ --epochs 150 --batch_size 25 --test_batch_size 25"
-
-    
-    main(argument.split())
+    main() #argument.split())
